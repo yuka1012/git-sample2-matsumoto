e5e429260965aa5d1a92eabd08219c46be7fe2af
matsumoto <yuka.matsumoto@rakus.co.jp>
2017-08-08 14:10:38 +0900

    新規プロジェクトを作成しました。

---
+++ src/main/java/jp/co/rakus/git_sample2_matsumoto/App.java
@@ -0,0 +1,13 @@
+package jp.co.rakus.git_sample2_matsumoto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
